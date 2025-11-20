2774f5303d29efa08314f8b564a88850d487c2d8
Aron de Jong <adj00@protonmail.com>
2025-11-20 15:45:32 +0100

    Add italic untitled fallback

--- src/components/song-view/Song.tsx
+++ src/components/song-view/Song.tsx
@@ -8,7 +8,7 @@ export function SongView({ song }: { song: Song }) {
   return (
     <>
       <Paper elevation={1} sx={{ padding: theme.spacing(1) }}>
-        <Typography variant="h3">{song.title}</Typography>
+        <Typography variant="h3">{song.title ?? <i>Untitled</i>}</Typography>
         <Typography variant="subtitle1">{song.subtitle}</Typography>
         <Typography variant="subtitle2" color="textSecondary">
           {song.artists.length > 0 ? "By " + song.artists.join(", ") : ""}
