cb44367ec06053d7fa05146c7add2ba8f2cd95d3
Aron de Jong <adj00@protonmail.com>
2025-11-21 17:33:33 +0100

    Add note playback to abc

--- src/components/abc-view.tsx
+++ src/components/abc-view.tsx
@@ -1,6 +1,6 @@
-import { useEffect, useRef } from "react";
+import React, { useEffect, useRef } from "react";
 import abcjs from "abcjs";
-import { Box } from "@mui/material";
+import { useTheme } from "@mui/material";
 
 import "abcjs/abcjs-audio.css";
 import "../assets/abcjs.css";
@@ -73,8 +73,9 @@ export function AbcView({
   visualParams,
 }: {
   abcCode: string;
-  visualParams: abcjs.AbcVisualParams;
+  visualParams?: abcjs.AbcVisualParams;
 }) {
+  const theme = useTheme();
   const controllerRef = useRef<HTMLDivElement>(null);
   const contentRef = useRef<HTMLDivElement>(null);
 
@@ -118,22 +119,47 @@ export function AbcView({
       ?.addEventListener("click", startTimer);
   }, []);
 
+  const clickListener: abcjs.ClickListener = async (abcElem: abcjs.AbcElem) => {
+    console.log("Clicked on element:", abcElem);
+    console.log("Audio contet", abcjs.synth.activeAudioContext());
+    await abcjs.synth.activeAudioContext().resume();
+    if (abcElem.midiPitches) {
+      abcjs.synth.playEvent(
+        abcElem.midiPitches,
+        abcElem.midiGraceNotePitches,
+        tune.current?.millisecondsPerMeasure() ?? 400,
+      );
+    }
+  };
+
   useEffect(() => {
-    const renderReturn = abcjs.renderAbc(
-      contentRef.current!,
-      abcCode,
-      visualParams,
-    );
-    const oldTune = tune.current;
+    if (!abcCode.trim()) {
+      contentRef.current!.innerHTML = "";
+      controllerRef.current!.innerHTML = "";
+      return;
+    }
+    const renderReturn = abcjs.renderAbc(contentRef.current!, abcCode, {
+      clickListener,
+      selectionColor: "var(--abcjs-selection-color, blue)",
+      ...visualParams,
+    });
     tune.current = renderReturn[0];
 
-    synthController.current.setTune(tune.current, oldTune !== null);
+    synthController.current.setTune(tune.current, false);
   }, [abcCode, visualParams]);
 
   return (
     <>
-      <Box ref={contentRef} />
-      <Box ref={controllerRef} />
+      <div
+        style={
+          {
+            "--abcjs-highlight-color": theme.palette.primary.main,
+            "--abcjs-selection-color": theme.palette.secondary.main,
+          } as React.CSSProperties
+        }
+        ref={contentRef}
+      />
+      <div ref={controllerRef} />
     </>
   );
 }
