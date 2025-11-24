c90a3f0bbcba38b56e8741328efcc781170a7cd9
Aron de Jong <jongade@protonmail.com>
2025-11-24 16:59:59 +0100

    Add missing key

--- src/components/song-view/lines/LyricsLine.tsx
+++ src/components/song-view/lines/LyricsLine.tsx
@@ -5,8 +5,9 @@ import { ChordLyricsPairView } from "../ChordLyricsPair";
 export function LyricsLineView({ line }: { line: LyricsLine }) {
   return (
     <Box sx={{ display: "flex" }}>
-      {line.pairs.map((pair) => (
+      {line.pairs.map((pair, index) => (
         <ChordLyricsPairView
+          key={index}
           chordLyricsPair={pair}
           showEmptyChord={line.pairs.length > 1}
         />
